3cd490f7362b124f4d3b8a18c3a97433d1b3c59c
DammyD <ibukunoluwadawodu@gmail.com>
2023-03-22 11:05:02 +0100

    setting up react

---
+++ 2.alternate-react-setup/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function-based component
+// A component must always have a return statement
+// It must always return a react element called react component(JSX)
+// JSX is the combination of html tags. It expands the ability of html(html on steriods)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+      React is very interesting...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
