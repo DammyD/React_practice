a90f73e9013f15a9d61f747f83bd65e148524ec3
DammyD <ibukunoluwadawodu@gmail.com>
2023-03-23 14:28:52 +0100

    jsx in react

---
+++ 3.jsx-in-react/src/Countries.js
@@ -0,0 +1,25 @@
+function ListItem(props) {
+    return <li>{props.name}</li>
+}
+
+function Countries() {
+
+    const countries = ['Namiba', 'Kenya', 'Egypt']
+    return (
+        <ul>
+            <ListItem name="Nigeria" />
+            <li>Nigeria</li>
+            <li>South Africa</li>
+            <li>Ghana</li>
+            <li>Niger</li>
+            <li>Botswana</li>
+            {countries.map((country, index) => {
+                return (
+                    <li key={index}>{country}</li>
+                )
+            })}
+        </ul>
+    )
+}
+
+export default Countries;
