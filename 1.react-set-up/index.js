6bb15211a7f76588060d5c42197313e3564766e2
DammyD <ibukunoluwadawodu@gmail.com>
2023-03-23 14:22:06 +0100

    setting up react

--- 1.react-set-up/index.js
+++ 1.react-set-up/index.js
@@ -15,3 +15,22 @@ const App = () => {
   const container = document.getElementById("root");
   const root = ReactDOM.createRoot(container);
   root.render(React.createElement(App));
+
+  // Attributes in JS are props in React
+  
+  // JSX
+  // Component
+  function Greeting() {
+    return (
+        <h1>JSX is superb!</h1>
+    );
+}
+// calling / instantiating component
+const element = <Greeting></Greeting>;
+
+
+// JavaScript
+// function Greeting() {
+//   return "Hello";
+// }
+// const greet = Greeting();
