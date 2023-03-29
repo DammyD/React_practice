671acd27c0d8a2a37dd5943f6fad3aebf06164ca
DammyD <ibukunoluwadawodu@gmail.com>
2023-03-29 02:22:26 +0100

    react component

---
+++ 4.react-components/src/components/BottomLeftmenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+
+const BottomLeftmenu = () => {
+  return (
+    <div>
+       <MenuItem title={"About"} />
+        <MenuItem title={"Advertising"} />
+        <MenuItem title={"Business"} />
+        <MenuItem title={"How Search works"} />
+    </div>
+  )
+}
+
+export default BottomLeftmenu
