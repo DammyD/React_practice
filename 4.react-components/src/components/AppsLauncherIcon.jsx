ca91a37f8db7f93e907d60c6a2b51426a0a584d8
DammyD <ibukunoluwadawodu@gmail.com>
2023-03-29 02:21:54 +0100

    react component

---
+++ 4.react-components/src/components/AppsLauncherIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import icon from "../images/app-launcher.png"
+
+const AppsLauncherIcon = () => {
+  return (
+    <div>
+        <img src={icon} alt="apps launcher" style={{height: "16px"}} />
+    </div>
+  )
+}
+
+export default AppsLauncherIcon;
