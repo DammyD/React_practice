cb747e8d8aa57ab2ddb8ebef2a0e39f33a2b2120
DammyD <ibukunoluwadawodu@gmail.com>
2023-03-29 02:23:14 +0100

    react component

---
+++ 4.react-components/src/components/BottomRightMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+
+const BottomRightMenu = () => {
+  return (
+    <div> <MenuItem title={"Privacy"} />
+    <MenuItem title={"Terms"} />
+    <MenuItem title={"Settings"} />
+    </div>
+  )
+}
+
+export default BottomRightMenu
